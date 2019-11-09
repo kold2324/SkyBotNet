ac38eddf49436c32dfcfec0dee82a17362af9987
MrPipin <suchkovkirill22@outlook.com>
2019-11-09 13:47:45 +0300

    First commit

---
+++ SkyBotNet/Settings/Config.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkyBotNet.Settings
+{
+    public class Config
+    {
+        public string Token { get; set; }
+
+        public string Prefix { get; set; }
+    }
+}
